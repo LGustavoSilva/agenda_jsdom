096659c1e94693c3259fc72f5446e35428cb1036
Luiz Gustavo Silva <gu.ado20@gmail.com>
2023-10-23 17:08:36 -0300

    melhoria no campo de numero do contato

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('form-contact')
 const nomeContato = []
-const numuroContato = []
+const numeroContato = []
 
 
 let linhas = ''
@@ -20,12 +20,12 @@ function inserirContato() {
     if (nomeContato.includes(inputNomeContato.value)) {
         alert(`o Contato ${inputNomeContato.value} já existe`)
     }
-    else if (numuroContato.includes(inputNumberContato.value)) {
+    else if (numeroContato.includes(inputNumberContato.value)) {
         alert(`o Número ${inputNumberContato.value} já pertence a outro Contato`)
     }
     else{
         nomeContato.push(inputNomeContato.value)
-        numuroContato.push(inputNumberContato.value)
+        numeroContato.push(inputNumberContato.value)
 
         let linha = '<tr>'
 
